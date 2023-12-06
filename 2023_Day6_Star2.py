0e3c744f01b954a481ed29c1fe65c656b8d32067
ITanmayee <67579192+ITanmayee@users.noreply.github.com>
2023-12-06 12:23:47 +0530

    2023 Day6 Star2

---
+++ 2023_Day6_Star2.py
@@ -0,0 +1,33 @@
+'''
+--- Part Two ---
+As the race is about to start, you realize the piece of paper with race times and record distances you got earlier actually just has very bad kerning. There's really only one race - ignore the spaces between the numbers on each line.
+
+So, the example from before:
+
+Time:      7  15   30
+Distance:  9  40  200
+...now instead means this:
+
+Time:      71530
+Distance:  940200
+Now, you have to figure out how many ways there are to win this single race. In this example, the race lasts for 71530 milliseconds and the record distance you need to beat is 940200 millimeters. You could hold the button anywhere from 14 to 71516 milliseconds and beat the record, a total of 71503 ways!
+
+How many ways can you beat the record in this one much longer race?
+
+Your puzzle answer was 32607562.
+'''
+
+# time = 71530
+# distance = 940200
+
+time = 49877895
+distance = 356137815021882
+
+def getBeatenRecordsProduct(time, distance):
+    beatenRecords = 0
+    for i in range(1, time):
+        if (time - i) * i > distance:
+            beatenRecords += 1
+    return beatenRecords
+
+print(getBeatenRecordsProduct(time, distance))
