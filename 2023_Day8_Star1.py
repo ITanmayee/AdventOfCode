3460d4e68ac267efc4720db38865ad2fc552c0e8
ITanmayee <67579192+ITanmayee@users.noreply.github.com>
2023-12-10 21:27:45 +0530

    Update 2023 Day8 Star1

--- 2023_Day8_Star1.py
+++ 2023_Day8_Star1.py
@@ -6,33 +6,35 @@ def getNodes(nodesInput):
         #nodeInfo = connectingNodes.split(', ')
         nodesList.append(node)
         #print(nodeInfo)
-        connectingNodesList.append((connectingNodes[1:4], connectingNodes[-3:-6:-1]))
+        connectingNodesList.append((connectingNodes[1:4], connectingNodes[-3:-6:-1][::-1]))
     # print('conn', connectingNodesList)
     return nodesList, connectingNodesList
 
 def stepsToReachZZZ(instructions, nodesList, connectingNodesList):
     node = nodesList[0]
+    instructionIndex, steps = 0, 0
     left, right = connectingNodesList[0][0], connectingNodesList[0][1]
-    instructionIndex, steps = 0, 1
-    while node != 'ZZZ':
-        print(node, left, right)
-        if instructions[instructionIndex] == 'R':
-            node = right
-        else:
-            node = left
-
-        left, right = connectingNodesList[nodesList.index(node)][0], connectingNodesList[nodesList.index(node)][1]
-        instructionIndex += 1
-        if instructionIndex == len(instructions) - 1:
-            instructionIndex = 0
-
+    while True:
+        if node == 'ZZZ':
+                print('yes', left, right)
+                break
         steps += 1
-    return steps + 1
+        if instructionIndex == len(instructions):
+                instructionIndex = 0
+        if instructions[instructionIndex] == 'R':
+                node = right
+        if instructions[instructionIndex] == 'L':
+                node = left
+        instructionIndex += 1
+        #print(node, instructions[instructionIndex])  
+        left, right = connectingNodesList[nodesList.index(node)][0], connectingNodesList[nodesList.index(node)][1]
+        print(left, right)
+    return steps
 
 def readInpText(filename):
     fileContent = open(filename, 'r')
     return fileContent.readlines()
 
-nodesList, connectingNodesList = getNodes(readInpText('inp11.txt'))
-# print(nodesList, connectingNodesList)
-print(stepsToReachZZZ('LLR', nodesList, connectingNodesList))
+nodesList, connectingNodesList = getNodes(readInpText('aocd3inp.txt'))
+print(nodesList, connectingNodesList)
+print(stepsToReachZZZ('LRRLRRRLRLRLLRRLRLRLLRLRLRLLLLRRRLLRRRLRRRLRRRLLRLLRLRRLRLRLRRRLLLLRRLRLRRLRRLLRRRLRRLRLRRLRRLRRLRRLRLLRRLRRLLLLRLRLRRLLRRLLRRLRLLRLRRLRRLRRLRRRLRRLLLRRLRRRLRLRRRLLRLRRLRRRLRRLLRRRLRRLRLLRRLLRRLRRLRRRLRRLLRRLRRRLRLRLRLRLRLRRLRRLLRRRLRLRRLRRRLRLRLRLRLRLRRRLRRLRRRLLRRLRLLRRRLRRLRLLLLRRRLRRLRRRR', nodesList, connectingNodesList))
