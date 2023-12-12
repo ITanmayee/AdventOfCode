d75f313807cdc3ca2befef525eb903f5d1219a66
ITanmayee <67579192+ITanmayee@users.noreply.github.com>
2023-12-12 12:51:57 +0530

    2023 Day9 Star2

---
+++ 2023_Day9_Star2.py
@@ -0,0 +1,43 @@
+'''
+--- Part Two ---
+Of course, it would be nice to have even more history included in your report. Surely it's safe to just extrapolate backwards as well, right?
+
+For each history, repeat the process of finding differences until the sequence of differences is entirely zero. Then, rather than adding a zero to the end and filling in the next values of each previous sequence, you should instead add a zero to the beginning of your sequence of zeroes, then fill in new first values for each previous sequence.
+
+In particular, here is what the third example history looks like when extrapolating back in time:
+
+5  10  13  16  21  30  45
+  5   3   3   5   9  15
+   -2   0   2   4   6
+      2   2   2   2
+        0   0   0
+Adding the new values on the left side of each sequence from bottom to top eventually reveals the new left-most history value: 5.
+
+Doing this for the remaining example data above results in previous values of -3 for the first history and 0 for the second history. Adding all three new values together produces 2.
+
+Analyze your OASIS report again, this time extrapolating the previous value for each history. What is the sum of these extrapolated values?
+
+Your puzzle answer was 990.
+'''
+
+def  readInpText(filename):
+    fileContent = open(filename, 'r')
+    return fileContent.readlines()
+
+def getHistoryReportValues(reportValues):
+    reportValues = list(map(int, reportValues.strip().split(' ')))
+    historyReportValues = [reportValues]
+    while set(reportValues) != {0}:
+        reportValues = [reportValues[i] - reportValues[i-1] for i in range(len(reportValues) - 1, 0, -1)][::-1]
+        historyReportValues.append(reportValues)
+    return historyReportValues[::-1]
+
+def getBeginingValuesSum(reportValues):
+    historyReportValues = getHistoryReportValues(reportValues)
+    termDiff = 0
+    for reportValue in historyReportValues:
+        termDiff = reportValue[0] - termDiff
+    return termDiff
+
+myInp = readInpText('inp9.txt')
+print(sum(map(getBeginingValuesSum, myInp)))
